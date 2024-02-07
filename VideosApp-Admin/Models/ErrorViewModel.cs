f36d1b2ffe241159611129f6e48eef84a930d285
ricochet01 <cromika123@hotmail.com>
2024-02-07 21:26:12 +0100

    Created an admin webpage with a paged list of countries

---
+++ VideosApp-Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VideosApp_Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
