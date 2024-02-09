3742728e153ab72dba473309356172da9bbe8c7f
ricochet01 <cromika123@hotmail.com>
2024-02-09 01:55:49 +0100

    Video page and video page details

--- VideoApp-Public/Controllers/HomeController.cs
+++ VideoApp-Public/Controllers/HomeController.cs
@@ -1,21 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using VideoApp_Public.Models;
+using VideosApp.Interface;
+using VideosApp.Model;
 
 namespace VideoApp_Public.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IVideoRepository videoRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IVideoRepository videoRepository)
         {
             _logger = logger;
+            this.videoRepository = videoRepository;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string searchString)
         {
-            return View();
+	        ViewData["CurrentFilter"] = searchString;
+
+	        var videos = videoRepository.GetAsyncVideos()
+		        .Include(v => v.Genre)
+		        .Include(v => v.Image)
+		        .OrderBy(v => v.CreatedAt);
+
+			if (!string.IsNullOrEmpty(searchString))
+	        {
+		        // We have to order by id every time to turn it into an IOrderedQueryable
+		        videos = videos.Where(v
+				        => v.Name.Contains(searchString))
+			        .OrderBy(v => v.Id);
+	        }
+
+			return View(videos);
         }
 
         public IActionResult Privacy()
@@ -23,7 +43,32 @@ namespace VideoApp_Public.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            var video = videoRepository.GetAsyncVideos()
+                .Include(v => v.Genre)
+                .Include(v => v.Image)
+                .FirstOrDefault(v => v.Id == id);
+
+            var tags = videoRepository.GetVideoTags(video.Id);
+            var videoTags = new List<VideoTag>();
+
+            foreach (var tag in tags)
+            {
+                videoTags.Add(new VideoTag()
+                {
+                    Tag = tag,
+                    Video = video
+                });
+            }
+
+            video.VideoTags = videoTags;
+
+            return View(video);
+        }
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
